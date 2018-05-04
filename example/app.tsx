0dbeebdc1ca66ba7253e87c9e4fe6409c18b3e4d
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-05-05 01:00:04 +1000

    Prettify example

--- example/app.tsx
+++ example/app.tsx
@@ -1,21 +1,90 @@
 import * as React from 'react';
 import {Component} from 'react';
-import {AppWrapper} from './styled';
+import VideoSnapshot from '../src';
+import Button from '@atlaskit/button';
+import FieldText from '@atlaskit/field-text';
+import {PreviewWrapper, TimeWrapper, ActionsWrapper, Preview, FileInput} from './styled';
 
-export interface AppState {
-  
+export interface AppProps {
+
 }
 
-export default class App extends Component <{}, AppState> {
+export interface AppState {
+  currentTime: number;
+  videoPreview?: string;
+}
+export default class App extends Component<AppProps, AppState> {
+  inputRef: HTMLInputElement;
+  snapshoter: VideoSnapshot;
+
   state: AppState = {
+    currentTime: 0,
+  }
+  
+  onChange = async (e: any) => {
+    const files = e.target.files;
+    this.snapshoter = new VideoSnapshot(files[0]);
+    const videoPreview = await this.snapshoter.takeSnapshot();
     
+    this.setState({
+      videoPreview
+    });
+  }
+  
+  onClick = () => {
+    this.inputRef.click();
+  }
+
+  onSnapshot = async (e: any) => {
+    const {currentTime} = this.state;
+    const videoPreview = await this.snapshoter.takeSnapshot(currentTime);
+    
+    this.setState({
+      videoPreview
+    });
+  }
+
+  saveInputRef = (e: any) => {
+    if (!e) return
+
+    this.inputRef = e;
+  }
+
+  onTimeChange = (e: any) => {
+    this.setState({
+      currentTime: e.target.value
+    });
   }
 
   render() {
+    const {videoPreview, currentTime} = this.state;
+    const hasPreview = !!videoPreview;
+    const img = videoPreview ? <Preview src={videoPreview} /> : null;
+    
     return (
-      <AppWrapper>
-        Example!
-      </AppWrapper>
-    )
+      <div>
+        <ActionsWrapper>
+          <Button appearance="primary" onClick={this.onClick} >Pick file</Button>
+          <FileInput innerRef={this.saveInputRef} type="file" onChange={this.onChange} />
+          {
+            hasPreview ? 
+            <TimeWrapper>
+              <FieldText 
+                value={`${currentTime}`}
+                compact={true}
+                isLabelHidden={true}
+                placeholder="snapshot time 00:00"
+                onChange={this.onTimeChange}
+              />
+              <Button appearance="primary" onClick={this.onSnapshot} >Take snapshot</Button>
+            </TimeWrapper>
+            : null
+          } 
+        </ActionsWrapper>
+        <PreviewWrapper>
+          {img}
+        </PreviewWrapper>
+      </div>
+    );
   }
 }
--- example/styled.ts
+++ example/styled.ts
@@ -1,5 +1,22 @@
 import styled from 'styled-components';
 
-export const AppWrapper = styled.div`
+export const Preview = styled.img`
+  width: 300px;
+  border-radius: 3px;
+`;
 
+export const FileInput = styled.input`
+  display: none;
+`;
+
+export const PreviewWrapper = styled.div`
+  margin-top: 10px;
+`;
+
+export const ActionsWrapper = styled.div`
+  display: flex;
+`;
+
+export const TimeWrapper = styled.div`
+  display: flex;
 `;
