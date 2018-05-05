600140ebfed9be259b4de54b5fd9d7bba04be0f1
Hector Leon Zarco Garcia <hezarco@gmail.com>
2018-05-05 14:39:16 +1000

    Add GH corner

--- example/styled.ts
+++ example/styled.ts
@@ -13,6 +13,12 @@ injectGlobal`
     border: 1px solid;
     margin: 5px;
   }
+  
+  .github-corner:hover .octo-arm{animation:octocat-wave 560ms ease-in-out}@keyframes octocat-wave{0%,100%{transform:rotate(0)}20%,60%{transform:rotate(-25deg)}40%,80%{transform:rotate(10deg)}}@media (max-width:500px){.github-corner:hover .octo-arm{animation:none}.github-corner .octo-arm{animation:octocat-wave 560ms ease-in-out}}
+
+  .github-corner svg {
+    fill:#70B7FD; color:#fff; position: absolute; top: 0; border: 0; right: 0;
+  }
 `;
 
 export const Preview = styled.img`
