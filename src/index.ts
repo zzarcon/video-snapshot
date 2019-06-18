9b258ef080d3d50dc565000b1223bac05468d2d0
tomieric <tomieric@gmail.com>
2019-06-18 17:01:23 +0800

    Feat: suport url,snapshot size

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export type CustomVideoTime = 'start' | 'middle' | 'end';
 export type VideoTime = number | CustomVideoTime;
+export type SnapshotRectUnit = number
+
 export interface Dimensions {
   width: number;
   height: number;
@@ -9,26 +11,30 @@ class VideoSnapshot {
   videoUrl: string;
 
   // TODO: Add support for base64?
-  constructor(blob: Blob) {
-    const url = URL.createObjectURL(blob);
+  constructor(blob: Blob | String) {
+    const url = typeof blob === 'string' ? blob : URL.createObjectURL(blob);
 
     this.videoUrl = url;
   }
 
   // TODO: Support time="1:20", time="01:05"
-  async takeSnapshot(time?: VideoTime): Promise<string> {
+  async takeSnapshot(time?: VideoTime, width?: SnapshotRectUnit, height?: SnapshotRectUnit): Promise<string> {
     const video = await this.loadVideo(time);
     const canvas = document.createElement('canvas');
 
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
+    canvas.width = width ? width : video.videoWidth;
+    canvas.height = height ? height : video.videoHeight;
     const context = canvas.getContext('2d');
 
     if (!context) {
       throw new Error('error creating canvas context');
     }
 
-    context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    if (width && height) {
+      context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, width, height);
+    } else {
+      context.drawImage(video, 0, 0, canvas.width, canvas.height);
+    }
 
     // TODO: better quality => toDataURL('image/jpeg', 1)
     const dataURL = canvas.toDataURL();
@@ -49,6 +55,7 @@ class VideoSnapshot {
       video.preload = 'metadata';
       video.src = this.videoUrl;
       video.muted = true;
+      video.setAttribute('crossOrigin', 'Anonymous');
 
       if (time === 0) {
         video.play();
